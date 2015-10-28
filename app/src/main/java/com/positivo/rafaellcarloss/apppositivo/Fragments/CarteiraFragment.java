73d63b909e89a32e9cd3d3ab7dd56deb34486562
RafaelCarlos <shanglangjunior@hotmail.com>
2015-10-28 16:48:50 -0300

    #1 - AppPositivo

---
+++ app/src/main/java/com/positivo/rafaellcarloss/apppositivo/Fragments/CarteiraFragment.java
@@ -0,0 +1,27 @@
+package com.positivo.rafaellcarloss.apppositivo.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by rafaellcarloss on 28/10/15.
+ */
+public class CarteiraFragment extends Fragment {
+
+    public CarteiraFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
