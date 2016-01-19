5d6cf3d2538f3e175816a326bcbdf1a1ed0c6a48
RafaelCarlos <shanglangjunior@hotmail.com>
2016-01-19 15:46:33 -0300

    #1 - AppPositivo

---
+++ app/src/main/java/com/positivo/rafaellcarloss/apppositivo/Conexao/OkHttp.java
@@ -0,0 +1,30 @@
+package com.positivo.rafaellcarloss.apppositivo.Conexao;
+
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+/**
+ * Created by rafaellcarloss on 19/01/16.
+ */
+public class OkHttp {
+
+    public static final MediaType XML
+            = MediaType.parse("application/json; charset=utf-8");
+
+    OkHttpClient client = new OkHttpClient();
+
+    public String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(XML, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+}
